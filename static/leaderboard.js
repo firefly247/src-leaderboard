5c8bc9903b1f5c192eadd4dab804612a04447f45
firefly247 <jangjg323@gmail.com>
2026-08-19 23:52:01 +0900

    Update Apps Script API endpoint

--- static/leaderboard.js
+++ static/leaderboard.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Public endpoint only. Keep GAS password and GitHub token in Script Properties.
-const API_URL = "https://script.google.com/macros/s/AKfycbxeOv94sfaAO_TfuyfQ5sLk33GtxKmIkQBL_LC3Q803C6ZMw0xQNCTuxdrYA5brwq_t/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwqffbCe9ERoqJx9i-czkTKtosURVksgnIlftSaP8A9w0KSzpsdL4wbsAIp7mCwFFHq/exec";
 const DATA_URL = "https://raw.githubusercontent.com/firefly247/src-leaderboard/main/data";
 const state = { events: [], records: [], rankings: {}, members: [], adminToken: sessionStorage.getItem("ergAdminToken") || "" };
 const $ = (s) => document.querySelector(s);
